1bc948c81ac97ae41f20c4dbceb81e58722d7578
Eric Hunsberger <erichuns@gmail.com>
2014-07-21 10:48:05 -0400

    Playing around with encoder learning using PES
    - doesn't work

--- encoder-learning.py
+++ encoder-learning.py
@@ -56,62 +56,88 @@ rng = np.random
 if 1:
     rf_shape = (9, 9)
     mask = create_mask(n_hid, (28, 28), rf_shape)
-    encoders = rng.normal(size=(n_hid, n_vis)) * mask
+    weights = rng.normal(size=(n_hid, n_vis)) * mask
 
-encoders = encoders.T
+weights = weights.T
 mask = mask.T
-encoders /= norm(encoders, axis=0, keepdims=True)
+weights /= norm(weights, axis=0, keepdims=True)
 
 neurons = nengo.LIF()
 gain, bias = neurons.gain_bias(200, -0.5)
 
 def encode(x):
-    return neurons.rates(np.dot(x, encoders), gain, bias)
+    return neurons.rates(np.dot(x, weights), gain, bias)
 
-# --- train the network
-n_epochs = 1
-batch_size = 10
-
-batches = train_images.reshape(-1, batch_size, train_images.shape[1])
-
-# determine initial decoders
+# --- determine initial decoders
 x = train_images[:1000]
 decoders, _ = nengo.decoders.LstsqL2()(encode(x), x)
 
-rate = 0.001
+# x = train_images[:1000]
+# A = encode(x)
+# dshape = (n_hid, n_vis)
+
+# def func(d):
+#     n = x.shape[0]
+#     xhat = np.dot(A, d.reshape(dshape))
+#     E = xhat - x
+#     error = 0.5 * (E**2).sum() / n
+#     grad = np.dot(A.T, E) / n
+#     return error, grad.flatten()
+
+# from scipy.optimize.lbfgsb import fmin_l_bfgs_b as lbfgsb
+# d0 = np.random.normal(scale=0.000001, size=dshape).flatten()
+# decoders, _, _ = lbfgsb(func, d0, maxiter=100, iprint=2)
+
+# --- train the network
+n_epochs = 1
+batch_size = 1
+
+# batches = train_images.reshape(-1, batch_size, train_images.shape[1])
+batches = train_images[None, :, :]
+
+w_rate = 0.0001
+# d_rate = 0.0000001
+d_rate = 0.00000001
+
+def test(x):
+    # test error
+    # x = test_images[:200]
+    a = encode(x)
+    xhat = np.dot(a, decoders)
+
+    plt.figure(99)
+    plt.clf()
+
+    plotting.compare(
+        [x.reshape(-1, 28, 28), xhat.reshape(-1, 28, 28)],
+        rows=5, cols=20, vlims=(-1, 1))
+    plt.draw()
+
+    print "error", rms(xhat - x, axis=1).mean()
+
+test(test_images[:200])
+
 
 for i in range(n_epochs):
     for x in batches:
 
         a = encode(x)
+        # print a.mean()
         xhat = np.dot(a, decoders)
         x_err = xhat - x
 
-        # update encoders
-        a_err = np.dot(x_err, encoders)
-        d_encoders = (rate / batch_size) * np.dot(x.T, a_err)
-        encoders += d_encoders
+        # update weights
+        a_err = np.dot(x_err, weights)
+        d_weights = -(w_rate / batch_size) * np.dot(x.T, a_err)
+        # weights += d_weights
 
         # update decoders
-        # d_decoders = (rate / batch_size) * np.dot(a.T, x_err)
-        # decoders += d_decoders
+        d_decoders = -(d_rate / batch_size) * np.dot(a.T, x_err)
+        decoders += d_decoders
 
-        x = train_images[:1000]
-        decoders, _ = nengo.decoders.LstsqL2()(encode(x), x)
+        # x = train_images[:1000]
+        # decoders, _ = nengo.decoders.LstsqL2()(encode(x), x)
 
-        # test error
-        x = test_images[:200]
-        a = encode(x)
-        xhat = np.dot(a, decoders)
-
-        plt.figure(99)
-        plt.clf()
-
-        plotting.compare(
-            [x.reshape(-1, 28, 28), xhat.reshape(-1, 28, 28)],
-            rows=5, cols=20, vlims=(-1, 1))
-        plt.draw()
-
-        print "error", rms(xhat - x, axis=1).mean()
+        test(test_images[:200])
 
         # raw_input("key?")
