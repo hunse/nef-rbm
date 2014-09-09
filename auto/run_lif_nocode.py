5c655d7f76ef76c7a146b073b28cba3f641d7372
Eric Hunsberger <erichuns@gmail.com>
2014-09-09 15:53:46 -0400

    Added a couple other classifiers to nocode

--- auto/run_lif_nocode.py
+++ auto/run_lif_nocode.py
@@ -65,13 +65,24 @@ n_labels = labels.size
 codes, layers = forward(test_images, weights, biases)
 classes = np.dot(codes, Wc) + bc
 inds = np.argmax(classes, axis=1)
-labels = np.unique(test_labels)
 errors = (test_labels != labels[inds])
 print "ANN error:", errors.mean()
 print "ANN classes: mean=%0.3f, std=%0.3f, min=%0.3f, max=%0.3f" % (
     classes.mean(), classes.std(0).mean(), classes.min(), classes.max())
 for i, layer in enumerate(layers):
     print "Layer %d: sparsity=%0.3f, %0.3f" % (i, (layer > 0).mean(), (layer > 1).mean())
+
+# try centroid classifier
+code_means = np.array([codes[test_labels == label].mean(0) for label in labels])
+dists = ((codes[:, :, None] - code_means.T)**2).sum(axis=1)
+errors = (test_labels != labels[np.argmin(dists, axis=1)])
+print "ANN centroid error:", errors.mean()
+
+# try dot product classifier
+code_mean = codes.mean(0)
+dists = np.dot(codes - code_mean, (code_means - code_mean).T)
+errors = (test_labels != labels[np.argmax(dists, axis=1)])
+print "ANN dot error:", errors.mean()
 
 if 1:
     plt.figure(101)
@@ -88,6 +99,7 @@ intercept = 0
 amp = 1. / max_rate
 assert np.allclose(neuron_type.gain_bias(max_rate, intercept), (1, 1), atol=1e-2)
 
+dt = 1e-3
 model = nengo.Network(seed=97)
 with model:
     input_images = nengo.Node(output=get_image, label='images')
@@ -107,7 +119,7 @@ with model:
                              transform=W.T, synapse=pstc)
         else:
             nengo.Connection(layers[-1].neurons, layer.neurons,
-                             transform=W.T * amp * 1000, synapse=pstc)
+                             transform=W.T * amp / dt, synapse=pstc)
 
         layers.append(layer)
 
@@ -116,21 +128,45 @@ with model:
     class_bias = nengo.Node(output=bc)
     nengo.Connection(class_bias, class_layer.input, synapse=0)
     nengo.Connection(layers[-1].neurons, class_layer.input,
-                     transform=Wc.T * amp * 1000, synapse=pstc)
+                     transform=Wc.T * amp / dt, synapse=pstc)
 
     test = nengo.Node(output=test_dots, size_in=n_labels)
     nengo.Connection(class_layer.output, test)
+
+    # --- make centroid classifier node
+    def centroid_test_fn(t, x):
+        i = int(t / presentation_time)
+        d = ((x - code_means)**2).sum(1)
+        return test_labels[i] == labels[np.argmin(d)]
+
+    centroid_test = nengo.Node(centroid_test_fn, size_in=layers[-1].n_neurons)
+    nengo.Connection(layers[-1].neurons, centroid_test,
+                     transform=amp / dt, synapse=pstc)
+
+    # --- make dot classifier node
+    def dot_test_fn(t, x):
+        i = int(t / presentation_time)
+        # d = np.dot(code_means, x)
+        d = np.dot(code_means - code_mean, x - code_mean)
+        return test_labels[i] == labels[np.argmax(d)]
+
+    dot_test = nengo.Node(dot_test_fn, size_in=layers[-1].n_neurons)
+    nengo.Connection(layers[-1].neurons, dot_test,
+                     transform=amp / dt, synapse=pstc)
 
     # --- make probes
     probe_layers = [nengo.Probe(layer, 'spikes') for layer in layers]
     probe_class = nengo.Probe(class_layer.output, synapse=0.03)
     probe_test = nengo.Probe(test, synapse=0.01)
+    probe_centroid = nengo.Probe(centroid_test, synapse=0.01)
+    probe_dot = nengo.Probe(dot_test, synapse=0.01)
 
 
 # --- simulation
-sim = nengo.Simulator(model)
-sim.run(100.)
-# sim.run(1.)
+sim = nengo.Simulator(model, dt=dt)
+sim.run(100., progress=True)
+# sim.run(5.)
+# sim.run(1., progress=True)
 
 t = sim.trange()
 
@@ -151,7 +187,7 @@ for i, image in enumerate(images):
 
 plt.figure(1)
 plt.clf()
-r, c = 5, 1
+r, c = 6, 1
 
 plt.subplot(r, c, 1)
 plt.imshow(allimage, aspect='auto', interpolation='none', cmap='gray')
@@ -174,11 +210,16 @@ plt.ylabel('layer 2 (200)')
 plt.subplot(r, c, 4)
 plt.plot(t, sim.data[probe_class])
 plot_bars()
-plt.xlabel('time [s]')
 plt.ylabel('class')
 
 plt.subplot(r, c, 5)
 plt.plot(t, sim.data[probe_test])
+plt.ylim([-0.1, 1.1])
+plot_bars()
+plt.ylabel('correct')
+
+plt.subplot(r, c, 6)
+plt.plot(t, sim.data[probe_dot])
 plt.ylim([-0.1, 1.1])
 plot_bars()
 plt.xlabel('time [s]')
@@ -186,12 +227,20 @@ plt.ylabel('correct')
 
 plt.tight_layout()
 
-# plt.savefig('run_lif.png')
+plt.savefig('run_lif_nocode.png')
 
 # --- compute error rate
 zblocks = sim.data[probe_test].reshape(-1, 100)[:, 50:]  # 50 ms blocks at end of each 100
 errors = np.mean(zblocks, axis=1) < 0.5
-print errors.mean()
+print "Neuron error:", errors.mean()
+
+zblocks = sim.data[probe_centroid].reshape(-1, 100)[:, 50:]
+errors = np.mean(zblocks, axis=1) < 0.5
+print "Neuron centroid error:", errors.mean()
+
+zblocks = sim.data[probe_dot].reshape(-1, 100)[:, 50:]
+errors = np.mean(zblocks, axis=1) < 0.5
+print "Neuron dot error:", errors.mean()
 
 # z2 = np.argmax(y, axis=1) == labels.repeat(100)
 # zblocks = z2.reshape(-1, 100)[:, 80:]  # 20 ms blocks at end of each 100
